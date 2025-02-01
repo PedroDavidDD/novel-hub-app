bfa27ba449c81c6b6740e438e62310f1933ed7d4
PedroDavidDD <u19207574@utp.edu.pe>
2025-02-01 03:12:32 -0500

    [upd-css-novels-page-genres]

--- src/app/novels/pages/novels-page/novels-page.component.ts
+++ src/app/novels/pages/novels-page/novels-page.component.ts
@@ -3,24 +3,27 @@ import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { INovel, NovelsService } from '../../services/novels.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-novels-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './novels-page.component.html',
-  styleUrl: './novels-page.component.css',
+  styleUrls: ['./novels-page.component.css'],
 })
 export class NovelsPageComponent {
   languages: string[] = ['Any', 'Chinese', 'Korean'];
   statuses: string[] = ['Any', 'Ongoing', 'Completed', 'Hiatus'];
   sortByOptions: string[] = ['Name', 'Popular', 'Chapters', 'New', 'Rating'];
+  genres: string[] = ['Drama', 'Non-Fiction', 'Fantasy', 'Sci-Fi', 'Romance', 'Adventure', 'Action'];
 
   selectedLanguage: string = 'Any';
   selectedStatus: string = 'Any';
   selectedSort: string = 'Name';
+  selectedGenres: string[] = [];  // Array para géneros seleccionados
 
-  novels$: Observable<INovel[]> = of([]); 
+  novels$: Observable<INovel[]> = of([]);
 
   constructor(private novelsService: NovelsService) {
     this.refreshNovels();
@@ -41,18 +44,37 @@ export class NovelsPageComponent {
     this.refreshNovels();
   }
 
+  filterByGenres(): void {
+    this.refreshNovels();
+  }
+
   private refreshNovels(): void {
     this.novels$ = this.novelsService.getNovels().pipe(
       map(novels => this.applyFilters(novels))
     );
   }
 
+  onGenreChange(genre: string, event: any): void {
+    if (event.target.checked) {
+      // Si el checkbox está marcado, añadir el género al array
+      this.selectedGenres.push(genre);
+    } else {
+      // Si el checkbox está desmarcado, eliminar el género del array
+      this.selectedGenres = this.selectedGenres.filter(item => item !== genre);
+    }
+    this.refreshNovels();
+  }
+
   private applyFilters(novels: INovel[]): INovel[] {
-    console.log(novels)
     let filteredNovels = novels.filter(novel => {
       const languageMatch = this.selectedLanguage === 'Any' || novel.language === this.selectedLanguage;
       const statusMatch = this.selectedStatus === 'Any' || novel.status === this.selectedStatus;
-      return languageMatch && statusMatch;
+
+      // Filtra según los géneros seleccionados
+      const genreMatch = this.selectedGenres.length === 0 || 
+      this.selectedGenres.every(genre => novel.genres.includes(genre));
+
+      return languageMatch && statusMatch && genreMatch;
     });
 
     const sortFunctions: { [key: string]: (a: INovel, b: INovel) => number } = {
