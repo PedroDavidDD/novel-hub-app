07dd192e0f7c708f031fb60ee6fd0aa4086c67a2
PedroDavidDD <u19207574@utp.edu.pe>
2025-02-07 01:38:24 -0500

    [UPD/ADD- components ADS/ASIDE]

--- src/app/features/novels/pages/layout-page/layout-page.component.ts
+++ src/app/features/novels/pages/layout-page/layout-page.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "../../../../shared/layout/footer/footer.component";
 import { NavbarComponent } from "../../../../shared/layout/navbar/navbar.component";
+import { AsideComponent } from '../../../../shared/layout/aside/aside.component';
+import { AdsComponent } from '../../../../ads/ads.component';
 
 @Component({
   selector: 'app-layout-page',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavbarComponent],
+  imports: [
+    RouterOutlet, 
+    FooterComponent, 
+    NavbarComponent,
+    AsideComponent,
+    AdsComponent,
+  ],
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.css',
 })
