e545dfe89a67767f5206589696a9e9992fc630f2
PedroDavidDD <u19207574@utp.edu.pe>
2025-02-18 10:28:08 -0500

    [add-usehash-test]

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     // provideRouter(routes),
     provideRouter(routes, withHashLocation()), // Aquí se habilita el uso de hash
-    provideHttpClient(),
-    
+    provideHttpClient(),    
+    // { provide: LocationStrategy, useClass: HashLocationStrategy } // useHash
   ]
 };
