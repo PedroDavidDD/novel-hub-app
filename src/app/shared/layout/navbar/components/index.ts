eea4c28463e41dc5504c0ef8d667c0b2c666d735
PedroDavidDD <u19207574@utp.edu.pe>
2025-02-21 21:17:51 -0500

    refactior-feat(navbar): reorganizar los componentes del navbar a una carpeta 'components' y agregar un componente search-navbar al mismo

---
+++ src/app/shared/layout/navbar/components/index.ts
@@ -0,0 +1,2 @@
+export * from './search-navbar/search-navbar.component';
+export * from './user-menu/user-menu.component';
--- src/app/shared/layout/navbar/navbar.component.ts
+++ src/app/shared/layout/navbar/navbar.component.ts
@@ -2,12 +2,17 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ThemeSwitcherComponent } from '../../../core/components';
 import { CommonModule } from '@angular/common';
-import { UserMenuComponent } from "./user-menu/user-menu.component";
+import { UserMenuComponent } from "./components";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, ThemeSwitcherComponent, CommonModule, UserMenuComponent],
+  imports: [
+    RouterLink, 
+    ThemeSwitcherComponent, 
+    CommonModule, 
+    UserMenuComponent,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   encapsulation: ViewEncapsulation.None,
