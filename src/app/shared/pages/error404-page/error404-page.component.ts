69d470df921a4e5c0b0135b0109c85efd730092e
Pedro David De la cruz Díaz <avariciatz@gmail.com>
2025-12-22 22:21:02 -0500

    Feat: Error 404 diseño

--- src/app/shared/pages/error404-page/error404-page.component.ts
+++ src/app/shared/pages/error404-page/error404-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
-@Component({ 
+@Component({
   selector: 'app-error404-page',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './error404-page.component.html',
   styles: [
   ]
